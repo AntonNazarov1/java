02c9e2528e049129a5ba16482b8082f42076a4d2
AntonNazarov1 <anazarov1@gmail.com>
2017-03-14 21:28:44 +0300

    first commit

---
+++ exercise2.java
@@ -0,0 +1,10 @@
+
+public class exercise2 {
+    public static void main (String[] args){
+        int a = 58;
+        int b = a%10;
+        int c = (a/10)%10;
+        System.out.println("sum = " +(b+c));
+    }
+
+}
