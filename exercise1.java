bd767bab747a399e94afe6b5b10d814e8f3051fa
AntonNazarov1 <anazarov1@gmail.com>
2017-03-18 19:05:00 +0300

    comm u2

--- exercise1.java
+++
@@ -1,9 +0,0 @@
-public class exercise1 {
-    public static void main (String[] args){
-        int a = 21;
-        int b = 8;
-        int c = a/b;
-        int d = a%b;
-        System.out.println( c +" and "+d);
-    }
-}
--- exercise3.java
+++
@@ -1,8 +0,0 @@
-public class exercise3 {
-    public static void main (String[] args){
-
-        double a = 3.1;
-        System.out.println(Math.round(a));
-
-        }
-}
