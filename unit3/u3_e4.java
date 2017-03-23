bd145e9c1bc5b16a361774cf7255de929525ca34
AntonNazarov1 <anazarov1@gmail.com>
2017-03-23 16:30:31 +0300

    1234

--- unit3/u3_e4.java
+++ unit3/u3_e4.java
@@ -1,12 +1,12 @@
-import java.util.Random;
+/*import java.util.Random;     // 1-й способ не доделан
 
 public class u3_e4 {
     public static void main(String[] args) {
-        Number car1 = new Number();
+        Numbers car1 = new Numbers();
         car1.generate();
     }
 }
-class Number{
+class Numbers{
     int car_number = (int) (Math.random()*899+100);
     int region = (int) (Math.random()*89+10);
     String[] letters = {"A","B","E","K","M","H","O","P","C","T","Y","X","D"};
@@ -18,26 +18,26 @@ class Number{
             break;
         }
     }
-}
+} */
 
-
-/*                         // без генерации букв
-import java.util.Random;
+import java.util.Random;         // второй способ
 
 public class u3_e4 {
     public static void main(String[] args) {
-        Number car1 = new Number();
+        Numbers car1 = new Numbers();
         car1.generate();
     }
 }
-class Number{
-    int car_number = (int) (Math.random()*899+100);
-    int region = (int) (Math.random()*89+10);
 
+class Numbers {
+        int car_number = (int) (Math.random()*899+100);
+        int region = (int) (Math.random()*89+10);
+        String[] letters = {"A","B","E","K","M","H","O","P","C","T","Y","X","D"};
 
-    void generate(){
-        System.out.println("M " +car_number+ " MM " +region);
-    }
+        void generate() {
+            System.out.print(letters[new Random().nextInt(letters.length)] + " " + car_number + " ");
+            System.out.print(letters[new Random().nextInt(letters.length)]);
+            System.out.print(letters[new Random().nextInt(letters.length)] + " " + region);
+        }
+
 }
-
-*/
